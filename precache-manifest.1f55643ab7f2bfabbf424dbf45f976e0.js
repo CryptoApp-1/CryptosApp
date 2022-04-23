68e7f0071639c85dc099cc6ec2d4799944f65515
Youzid <terrorblad777@gmail.com>
2022-04-23 20:18:37 +0200

    Updates

---
+++ precache-manifest.1f55643ab7f2bfabbf424dbf45f976e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66f22613b340470e1364",
+    "url": "/CryptosApp/static/css/main.3e2cb6be.chunk.css"
+  },
+  {
+    "revision": "66f22613b340470e1364",
+    "url": "/CryptosApp/static/js/main.66f22613.chunk.js"
+  },
+  {
+    "revision": "b01d423f06da320d24dd",
+    "url": "/CryptosApp/static/css/1.3fbe42aa.chunk.css"
+  },
+  {
+    "revision": "b01d423f06da320d24dd",
+    "url": "/CryptosApp/static/js/1.b01d423f.chunk.js"
+  },
+  {
+    "revision": "b3e1f6ad0b89619843bb",
+    "url": "/CryptosApp/static/js/runtime~main.b3e1f6ad.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/CryptosApp/static/media/cryptocurrency.88532732.png"
+  },
+  {
+    "revision": "14880fc69febe71b1b3b488c2f32e2eb",
+    "url": "/CryptosApp/index.html"
+  }
+];
